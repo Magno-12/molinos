87c78740293c2e0d2746c78457dc5270b4231866
MagnoDEv <magno.martinez@alternova.com>
2023-07-17 08:00:01 -0500

    fix5

--- molino_project/settings.py
+++ molino_project/settings.py
@@ -172,8 +172,9 @@ AUTH_USER_MODEL = 'users.User'
 
 AUTHENTICATION_BACKENDS = ['apps.authentication.email_back.EmailBackend']
 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    '*',
+    'http://localhost:3000',
 ]
 
 ACCESS_TOKEN_EXPIRATION_MINUTES = 60
